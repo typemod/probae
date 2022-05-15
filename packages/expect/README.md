88860050acac4779d32f8894730533a26cbd6c34
Wyatt-Stanke <wyattstanke@icloud.com>
2022-05-15 11:43:16 -0400

    More updates

---
+++ packages/expect/README.md
@@ -0,0 +1,18 @@
+# @vvft/expect ✨🧪✨
+
+`@vvft/expect` is a package for simple assertions!
+
+- 📈 Simple 1-function API!
+- ⚡ No-friction greased-lightning speed!
+- 💎 Fancy colors!
+- 💡 100% Typescript!
+
+## Example
+
+```typescript
+import expect from "@vvft/expect"
+
+expect("test").toBe("test")
+
+expect("test").not().toBe("test") // Errors!
+```
--- packages/expect/tests/Expect.test.ts
+++ packages/expect/tests/Expect.test.ts
@@ -1,14 +1,16 @@
-import expect from "../src/index";
+import expect from '../src/index'
 
 import { test } from 'uvu'
 import * as assert from 'uvu/assert'
 
 // Tests here can be lighter because of all the testing for the ExpectObject class.
 
-test("expect", () => {
-	expect("test").toBe("test")
-	expect("test").not().toBe("testn't")
+test('expect', () => {
+	expect('test').toBe('test')
+	expect('test').not().toBe("testn't")
 
-	assert.throws(() => expect("test").toBe("testn't"))
-	assert.throws(() => expect("test").not().toBe("test"))
+	assert.throws(() => expect('test').toBe("testn't"))
+	assert.throws(() => expect('test').not().toBe('test'))
 })
+
+test.run()
